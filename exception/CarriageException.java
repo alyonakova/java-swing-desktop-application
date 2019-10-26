8d7faa14670f29b5594ba70535287a3796bc8fd5
Alyona Kovalyova <alyona@banka.space>
2019-10-26 18:32:52 +0300

    Delete unnecessary file

--- exception/CarriageException.java
+++
@@ -1,8 +0,0 @@
-package exception;
-
-public class CarriageException extends Exception {
-    @Override
-    public String getMessage() {
-        return "Already seen all the carriages of the train";
-    }
-}
